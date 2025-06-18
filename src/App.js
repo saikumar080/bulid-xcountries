7256d3b25aa095aa6d4698ba4153e6b12968ec20
saikumar pillalamarri <pillalamarrisaikumar8@gmail.com>
2025-06-18 11:31:52 +0530

    build-XCountries

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>I am building X-Countries Flags</h1>
+      <h1>I am building X-Countries Flag</h1>
     </div>
   );
 }
